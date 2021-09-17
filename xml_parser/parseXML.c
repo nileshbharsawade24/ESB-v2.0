a49a0f83bc0dc1f50bd461a8fb06aa7d54805db7
rohitbhamu <rohitbhamu6@gmail.com>
2021-09-17 09:11:54 +0530

    added xml_parser

---
+++ xml_parser/parseXML.c
@@ -0,0 +1,74 @@
+
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+
+xmlDocPtr load_xml_doc(char *xml_file_path) {
+    xmlDocPtr doc = xmlParseFile(xml_file_path);
+    if (doc == NULL) {
+        fprintf(stderr, "ERROR: Document not parsed successfully. \n");
+        return NULL;
+    }
+    return doc;
+}
+
+/**
+ * Extract the nodes matching the given xpath from the supplied
+ * XML document object.
+ */
+xmlXPathObjectPtr get_nodes_at_xpath(xmlDocPtr doc, xmlChar *xpath) {
+
+    xmlXPathContextPtr context = xmlXPathNewContext(doc);
+    if (context == NULL) {
+        printf("ERROR: Failed to create xpath context from the XML document.\n");
+        return NULL;
+    }
+    xmlXPathObjectPtr result = xmlXPathEvalExpression(xpath, context);
+    xmlXPathFreeContext(context);
+    if (result == NULL) {
+        printf("ERROR: Failed to evaluate xpath expression.\n");
+        return NULL;
+    }
+    if (xmlXPathNodeSetIsEmpty(result->nodesetval)) {
+        xmlXPathFreeObject(result);
+        printf("No matching nodes found at the xpath.\n");
+        return NULL;
+    }
+    return result;
+}
+
+/**
+ * Returns the text value of an XML element. It is expected that
+ * there is only one XML element at the given xpath in the XML.
+ */
+xmlChar* get_element_text(char *node_xpath, xmlDocPtr doc) {
+    xmlChar *node_text;
+    xmlXPathObjectPtr result = get_nodes_at_xpath(doc, 
+        (xmlChar*)node_xpath);
+    if (result) {
+        xmlNodeSetPtr nodeset = result->nodesetval;
+        if (nodeset->nodeNr == 1) {
+            node_text = xmlNodeListGetString(doc,
+                nodeset->nodeTab[0]->xmlChildrenNode, 1);
+        } else {
+            printf("ERROR: Expected one %s node, found %d\n", node_xpath, nodeset->nodeNr);
+        }
+        xmlXPathFreeObject(result);
+    } else {
+        printf("ERROR: Node not found at xpath %s\n", node_xpath);
+    }
+    return node_text;
+}
+
+
+int main(int argc, char **argv) {
+    char *docname = "/home/rohit/Camel/xml_parser/recieved_bmd.xml";
+    xmlDocPtr doc = load_xml_doc(docname);
+    printf("MessageID=%s\n", get_element_text("//MessageID", doc));
+    printf("Sender=%s\n", get_element_text("//Sender", doc));
+    printf("Destination=%s\n", get_element_text("//Destination", doc));
+    printf("MessageType=%s\n", get_element_text("//MessageType", doc));
+    printf("Payload=%s\n", get_element_text("//Payload", doc));
+    xmlFreeDoc(doc);
+    xmlCleanupParser();
+    return 1;
+}
